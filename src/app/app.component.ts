b6ebfd973e396771f87b2cc92b31a4073bf5b07d
DelMoral <delmoralvera@gmail.com>
2021-06-01 14:33:22 +0200

    EVOL|THEME| cambio dark/light theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'portfolio-delmo';
+  theme = 'dark';
+
+  changeTheme(): void {
+    this.theme === 'dark' ? this.theme = 'light' : this.theme = 'dark';
+  }
 }
